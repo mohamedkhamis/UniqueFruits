f09afaa07afc68d2069a372b1f2ce570fa824ea6
Mohamed Khamis <mohamed.khamis.alex@alexu.edu.eg>
2022-06-05 16:09:32 +0200

    Add project files.

---
+++ UniqueFruits/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var fruitsClass = new UniqueFruits.UniqueFruits();
+
+fruitsClass.UniqueFruitsLogic();
---
+++ UniqueFruits/UniqueFruits.cs
@@ -0,0 +1,38 @@
+﻿namespace UniqueFruits
+{
+    internal class UniqueFruits
+    {
+
+        public List<string>? EnumerableString { get; set; }
+        public void UniqueFruitsLogic()
+        {
+            string[] a1 = { "Watermelon", "Apple", "Mango", "Guava", "Banana" };
+            string[] a2 = { "Orange", "Kiwi", "Apple", "Watermelon" };
+            EnumerableString = new List<string>();
+            EnumerableString.AddRange(a1);
+            EnumerableString.AddRange(a2); 
+            foreach (var item in EnumerableString) 
+            { 
+                if (!IsExist(item)) 
+                { 
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public bool IsExist( string fruitItem)
+        {
+            var counter = 0;
+            if (EnumerableString == null) return false;
+            foreach (var arrayItem in EnumerableString)
+            {
+                if (arrayItem != fruitItem) continue;
+                counter++;
+                if (counter > 1) break;
+            }
+
+
+            return counter > 1;
+        }
+    }
+}
